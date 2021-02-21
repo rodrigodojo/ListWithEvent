b7c452395fd43120e9c088cecb3caaf3a0e96dce
rodrigodojo <rodrigodoj@gmail.com>
2021-02-21 17:08:58 -0300

    adicionado efeitos na SegundaTela.java

--- app/src/main/java/com/dojo/mylistimage/MainActivity.java
+++ app/src/main/java/com/dojo/mylistimage/MainActivity.java
@@ -1,24 +1,156 @@
 package com.dojo.mylistimage;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    String[] listaNomes = {"Felpudo","Fofura","Lesmo","Bugada","Uruca","Racing","iOS","Android","RealidadeAumentada",
+            "Sound FX","3D Studio Max", "Games"};
+
+    int[] listaIcone = {R.drawable.felpudo, R.drawable.fofura,R.drawable.lesmo,R.drawable.bugado,R.drawable.uruca,
+            R.drawable.carrinho, R.drawable.ios,R.drawable.android,R.drawable.realidade_aumentada,R.drawable.sound_fx,
+            R.drawable.max, R.drawable.games,};
+
+    String[] listaDescricao= {"O protagonista dos nossos cursos de iOS e Android. Vive se metendo em altas aventuras para aprender a programar.",
+            "É a namoradinha do Felpudo, a personagem que mostra que programação é sim coisa de meninas!",
+            "Uma criatura asqueirosa que vive atrapalhando a vida do Felpudo e riando problemas no seu caminho.",
+            "É o protagonista dos nossos cursos de iOS e Android. Vive se metendo em altas aventuras para aprender a programar.",
+            "É a namoradinha do Felpudo, a personagem que mostra que programação é sim coisa de meninas!",
+            "É a criatura asqueirosa que vive atrapalhando a vida do Felpudo e riando problemas no seu caminho.",
+            "É o protagonista dos nossos cursos de iOS e Android. Vive se metendo em altas aventuras para aprender a programar.",
+            "É a namoradinha do Felpudo, a personagem que mostra que programação é sim coisa de meninas!",
+            "É a criatura asqueirosa que vive atrapalhando a vida do Felpudo e riando problemas no seu caminho.",
+            "É o protagonista dos nossos cursos de iOS e Android. Vive se metendo em altas aventuras para aprender a programar.",
+            "É a namoradinha do Felpudo, a personagem que mostra que programação é sim coisa de meninas!",
+            "É a criatura asqueirosa que vive atrapalhando a vida do Felpudo e riando problemas no seu caminho."};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView minhaLista = findViewById(R.id.minhaLista);
+
+        MeuAdaptador meuAdaptador;
+        meuAdaptador = new MeuAdaptador(getApplicationContext(),R.layout.minha_celula);
+
+        int i=0;
+        for(String nome : listaNomes ){
+            DadosPersonagem dadosPersonagem;
+            dadosPersonagem = new DadosPersonagem(listaIcone[i],nome,listaDescricao[i]);
+            meuAdaptador.add(dadosPersonagem);
+            i++;
+        }
+
+        minhaLista.setAdapter(meuAdaptador);
+
+        minhaLista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                DadosPersonagem dadosPersonagem;
+                dadosPersonagem = (DadosPersonagem) meuAdaptador.getItem(position);
+
+                Intent intent = new Intent(MainActivity.this,SegundaTela.class);
+                intent.putExtra("nome",dadosPersonagem.getTitulo());
+                intent.putExtra("descricao",dadosPersonagem.getDescricao());
+                intent.putExtra("icone",dadosPersonagem.getIcone());
+                startActivity(intent);
+
+            }
+        });
+    }
+}
+
+class ViewPersonagem{
+    ImageView icone;
+    TextView titulo;
+    TextView descricao;
+}
+
+class DadosPersonagem{
+    private int icone;
+    private String titulo;
+    private String descricao;
+
+    public DadosPersonagem(int icone, String nome, String descricao) {
+        this.icone = icone;
+        this.titulo = nome;
+        this.descricao = descricao;
     }
 
-    public void clicou(View view){
-        Toast.makeText(this, "clicou!", Toast.LENGTH_SHORT).show();
-        Intent intent = new Intent(MainActivity.this,SegundaTela.class);
-        intent.putExtra("nome","Rodrigo Dojo");
-        startActivity(intent);
+    public int getIcone() {
+        return icone;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+}
+
+class MeuAdaptador extends ArrayAdapter {
+
+    public MeuAdaptador(@NonNull Context context, int resource) {
+        super(context, resource);
+    }
+
+    @Override
+    public void add(@Nullable Object object) {
+        super.add(object);
+    }
+
+    @Override
+    public int getCount() {
+        return super.getCount();
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        View minhaView;
+        minhaView = convertView;
+        ViewPersonagem viewPersonagem;
+
+        if (convertView == null) {
+            LayoutInflater inflater = (LayoutInflater) this.getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            minhaView = inflater.inflate(R.layout.minha_celula, parent, false);
+            viewPersonagem = new ViewPersonagem();
+
+            viewPersonagem.icone = minhaView.findViewById(R.id.meuIcone);
+            viewPersonagem.titulo = minhaView.findViewById(R.id.meuNome);
+
+            minhaView.setTag(viewPersonagem);
+        } else {
+            viewPersonagem = (ViewPersonagem) minhaView.getTag();
+        }
+
+        DadosPersonagem dadosPersonagem;
+        dadosPersonagem = (DadosPersonagem) this.getItem(position);
+
+        viewPersonagem.icone.setImageResource(dadosPersonagem.getIcone());
+        viewPersonagem.titulo.setText(dadosPersonagem.getTitulo());
+
+        return minhaView;
     }
 }
--- app/src/main/java/com/dojo/mylistimage/SegundaTela.java
+++ app/src/main/java/com/dojo/mylistimage/SegundaTela.java
@@ -2,8 +2,13 @@ package com.dojo.mylistimage;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 public class SegundaTela extends AppCompatActivity {
 
@@ -12,11 +17,22 @@ public class SegundaTela extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_segunda_tela);
 
+        ImageView icone = findViewById(R.id.meuIcone);
+        TextView nome = findViewById(R.id.txtNome);
+        TextView desc = findViewById(R.id.txtDescr);
+
+
         Bundle extras = getIntent().getExtras();
 
         if(extras != null){
-            String texto = extras.getString("nome");
-            Toast.makeText(this,""+texto,Toast.LENGTH_SHORT).show();
+            String txtNome = extras.getString("nome");
+            String txtDescr = extras.getString("descricao");
+            int iconeid = extras.getInt("icone");
+
+            icone.setImageResource(iconeid);
+            nome.setText(txtNome);
+            desc.setText(txtDescr);
         }
+
     }
 }
